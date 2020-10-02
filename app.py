cbbaa921dd841cc38e91a3fe2748995f9b6b02ce
Jack Wu <masterjack1997@gmail.com>
2020-10-02 14:59:53 -0400

    search done

--- app.py
+++ app.py
@@ -168,14 +168,31 @@ def search_venues():
   # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
   # seach for Hop should return "The Musical Hop".
   # search for "Music" should return "The Musical Hop" and "Park Square Live Music & Coffee"
-  response={
-    "count": 1,
-    "data": [{
-      "id": 2,
-      "name": "The Dueling Pianos Bar",
-      "num_upcoming_shows": 0,
-    }]
+
+  search_term = request.form.get('search_term', '').strip()
+  venues = Venue.query.filter(Venue.name.ilike('%' + search_term + '%')).all()
+
+  venue_list = [] 
+
+  for venue in venues: 
+    venue_list.append({
+      "id": venue.id, 
+      "name": venue.name
+    })
+
+  response = {
+    "count": len(venues), 
+    "data": venue_list
   }
+
+  # response={
+  #   "count": 1,
+  #   "data": [{
+  #     "id": 2,
+  #     "name": "The Dueling Pianos Bar",
+  #     "num_upcoming_shows": 0,
+  #   }]
+  # }
   return render_template('pages/search_venues.html', results=response, search_term=request.form.get('search_term', ''))
 
 @app.route('/venues/<int:venue_id>')
@@ -338,14 +355,31 @@ def search_artists():
   # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
   # seach for "A" should return "Guns N Petals", "Matt Quevado", and "The Wild Sax Band".
   # search for "band" should return "The Wild Sax Band".
-  response={
-    "count": 1,
-    "data": [{
-      "id": 4,
-      "name": "Guns N Petals",
-      "num_upcoming_shows": 0,
-    }]
+
+  search_term = request.form.get('search_term', '').strip()
+  artists = Artist.query.filter(Artist.name.ilike('%' + search_term + '%')).all()
+
+  artist_list = [] 
+
+  for artist in artists: 
+    artist_list.append({
+      "id": artist.id, 
+      "name": artist.name
+    })
+
+  response = {
+    "count": len(artists), 
+    "data": artist_list
   }
+
+  # response={
+  #   "count": 1,
+  #   "data": [{
+  #     "id": 4,
+  #     "name": "Guns N Petals",
+  #     "num_upcoming_shows": 0,
+  #   }]
+  # }
   return render_template('pages/search_artists.html', results=response, search_term=request.form.get('search_term', ''))
 
 @app.route('/artists/<int:artist_id>')
